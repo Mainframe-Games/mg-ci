7ca152ca0fde2c1aae3c956fc2aace6d4d5fb47d
brogan89 <brogan.king@outlook.com>
2023-03-23 14:46:50 +1300

    Added converting of type based on type string value from unity config

--- Deployment/Server/Unity/UnityRemoteConfigRequest.cs
+++ Deployment/Server/Unity/UnityRemoteConfigRequest.cs
@@ -26,11 +26,27 @@ public class UnityRemoteConfigRequest : UnityWebRequest
 
 		foreach (var prop in (JArray)body["value"])
 		{
-			if (prop["key"]?.ToString() == key)
-				prop["value"] = new JValue(value);
+			if (prop["key"]?.ToString() != key)
+				continue;
+			
+			var type = prop["type"]?.ToString();
+			var convertedValue = ConvertType(type, value);
+			prop["value"] = convertedValue;
 		}
 
 		await Web.SendAsync(HttpMethod.Put, url, AuthToken, body);
 		Logger.Log($"Remote Config key: {key} updated to value: {value}");
+	}
+
+	private static JValue ConvertType(string? type, object? value)
+	{
+		return type switch
+		{
+			"string" => new JValue(Convert.ToString(value)),
+			"int" => new JValue(Convert.ToInt32(value)),
+			"float" => new JValue(Convert.ToSingle(value)),
+			"bool" => new JValue(Convert.ToBoolean(value)),
+			_ => throw new ArgumentException($"Type not supported '{type}'")
+		};
 	}
 }
