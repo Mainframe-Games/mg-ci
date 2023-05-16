3d99143e159e4cc1699a505ea0cc849d2bfaab0a
brogan89 <brogan.king@outlook.com>
2023-05-17 09:16:02 +1200

    Added GetValue override to BuildSettingsAsset YAML

--- Builds/Configs/BuildSettingsAsset.cs
+++ Builds/Configs/BuildSettingsAsset.cs
@@ -7,4 +7,9 @@ public class BuildSettingsAsset : Yaml
 	public BuildSettingsAsset(string? path) : base(path)
 	{
 	}
+
+	public override T GetValue<T>(string path)
+	{
+		return base.GetValue<T>($"MonoBehaviour.{path}");
+	}
 }
