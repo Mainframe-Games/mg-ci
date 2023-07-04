3c920f8031cb5dc9ffd2eccb92761de197cff36e
brogan89 <brogan.king@outlook.com>
2023-07-05 10:22:22 +1200

    Added quick deploy window

---
+++ Unity/BuildSystem/Runtime/InternalVersion.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace BuildSystem
+{
+	/// <summary>
+	/// Used to diff versions built locally and streamline deployed to clanforge
+	/// </summary>
+	public static class InternalVersion
+	{
+		public static string Version
+		{
+			get => File.Exists(Path) ? File.ReadAllText(Path) : Application.version;
+			private set => File.WriteAllText(Path, value);
+		}
+
+		private static string Path => $"{Application.streamingAssetsPath}/internal-version.txt";
+
+		private static int _ver
+		{
+			get => PlayerPrefs.GetInt(nameof(InternalVersion), 0);
+			set => PlayerPrefs.SetInt(nameof(InternalVersion), value);
+		}
+
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+		public static void Load()
+		{
+			if (Version != Application.version)
+				Debug.Log($"Internal Version: {Version}");
+		}
+
+		public static void Bump()
+		{
+			Version = $"{Application.version}.{++_ver}";
+			Debug.Log($"Internal Version Bump: {Version}");
+		}
+	}
+}
