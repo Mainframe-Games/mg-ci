8a49a90b3a17bd318587b6819f58f39689d586a9
brogan89 <brogan.king@outlook.com>
2023-07-13 15:51:19 +1200

    Added server info discord command

--- DiscordBot/Commands/DocsCommand.cs
+++ DiscordBot/Commands/DocsCommand.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using Discord.WebSocket;
 
 namespace DiscordBot.Commands;
@@ -9,17 +8,8 @@ public class DocsCommand : Command
 	public override string? CommandName => "docs";
 	public override string? Description => "Takes you to docs page";
 	
-	public static void OpenUrl()
-	{
-		if (OperatingSystem.IsMacOS())
-			Process.Start("open", DOCS_URL);
-		else
-			Process.Start(new ProcessStartInfo(DOCS_URL) { UseShellExecute = true });
-	}
-
 	public override async Task ExecuteAsync(SocketSlashCommand command)
 	{
-		Process.Start(DOCS_URL);
-		await command.RespondAsync($"Opened {DOCS_URL}");
+		await command.RespondAsync(DOCS_URL);
 	}
 }
---
+++ DiscordBot/Commands/ServerInfoCommand.cs
@@ -0,0 +1,16 @@
+﻿using Discord.WebSocket;
+using SharedLib;
+
+namespace DiscordBot.Commands;
+
+public class ServerInfoCommand : Command
+{
+	public override string? CommandName => "server-info";
+	public override string? Description => "Returns version and uptime of server";
+	public override async Task ExecuteAsync(SocketSlashCommand command)
+	{
+		await command.DeferAsync();
+		var res = await Web.SendAsync(HttpMethod.Get, $"{DiscordWrapper.Config.BuildServerUrl}/info");
+		await command.RespondAsync(res.Content);
+	}
+}
