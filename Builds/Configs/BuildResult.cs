47abb1e9317ca9c248c046761dc7e1ccc079ff4a
brogan89 <brogan.king@outlook.com>
2023-08-17 17:53:22 +1200

    Added ToHourMinSecString to build result string

--- Builds/Configs/BuildResult.cs
+++ Builds/Configs/BuildResult.cs
@@ -10,6 +10,6 @@ public class BuildResult
 
 	public override string ToString()
 	{
-		return $"{BuildName} {BuildTime} ({BuildSize.ToByteSizeString()})";
+		return $"{BuildName} {BuildTime.ToHourMinSecString()} ({BuildSize.ToByteSizeString()})";
 	}
 }
