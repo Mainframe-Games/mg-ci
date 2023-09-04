467e63b591ec109f44d74db66d3ba5a1baa9e8a4
brogan89 <brogan.king@outlook.com>
2023-09-05 10:41:44 +1200

    Added extra try/catch at higher level in remote build request

--- Server/RemoteBuild/RemoteBuildTargetRequest.cs
+++ Server/RemoteBuild/RemoteBuildTargetRequest.cs
@@ -35,36 +35,47 @@ public class RemoteBuildTargetRequest : IProcessable
 
 	private async Task ProcessAsync(Workspace workspace)
 	{
-		// this needs to be here to kick start the thread, otherwise it will stall app
-		await Task.Delay(1);
-
-		Environment.CurrentDirectory = workspace.Directory;
-
-		if (Packet.CleanBuild)
-			workspace.CleanBuild();
-
-		workspace.Clear();
-		workspace.Update();
-		workspace.SwitchBranch(Packet.Branch);
-		workspace.Update(Packet.ChangesetId);
-
-		if (!Directory.Exists(workspace.Directory))
-			throw new DirectoryNotFoundException($"Directory doesn't exist: {workspace.Directory}");
-
-		// set build version in project settings
-		var projWriter = new ProjectSettings(workspace.ProjectSettingsPath);
-		projWriter.ReplaceVersions(Packet.BuildVersion);
-
-		foreach (var build in Packet.Builds)
+		try
 		{
-			var asset = workspace.GetBuildTarget(build.Value.AssetName);
-			var deployProcess = GetDeployProcess(build.Value, asset);
-			await StartBuilderAsync(Packet.PipelineId, build.Key, asset, workspace, deployProcess);
-		}
+			// this needs to be here to kick start the thread, otherwise it will stall app
+			await Task.Delay(1);
 
-		// clean up after build
-		workspace.Clear();
-		App.DumpLogs();
+			Environment.CurrentDirectory = workspace.Directory;
+
+			if (Packet.CleanBuild)
+				workspace.CleanBuild();
+
+			workspace.Clear();
+			workspace.Update();
+			workspace.SwitchBranch(Packet.Branch);
+			workspace.Update(Packet.ChangesetId);
+
+			if (!Directory.Exists(workspace.Directory))
+				throw new DirectoryNotFoundException($"Directory doesn't exist: {workspace.Directory}");
+
+			// set build version in project settings
+			var projWriter = new ProjectSettings(workspace.ProjectSettingsPath);
+			projWriter.ReplaceVersions(Packet.BuildVersion);
+
+			foreach (var build in Packet.Builds)
+			{
+				var asset = workspace.GetBuildTarget(build.Value.AssetName);
+				var deployProcess = GetDeployProcess(build.Value, asset);
+				await StartBuilderAsync(Packet.PipelineId, build.Key, asset, workspace, deployProcess);
+			}
+
+			// clean up after build
+			workspace.Clear();
+		}
+		catch (Exception e)
+		{
+			Logger.Log(e);
+			await SendExceptionToMaster(e);
+		}
+		finally
+		{
+			App.DumpLogs();
+		}
 	}
 	
 	/// <summary>
@@ -95,12 +106,12 @@ public class RemoteBuildTargetRequest : IProcessable
 		try
 		{
 			var builder = new LocalUnityBuild(workspace);
-			await SendToMasterServerAsync(pipelineId, buildIdGuid, asset.Name, null, BuildTaskStatus.Pending);
+			await SendToMasterServerAsync(buildIdGuid, asset.Name, null, BuildTaskStatus.Pending);
 			var result = builder.Build(asset);
 
 			if (result.IsErrors)
 			{
-				await SendToMasterServerAsync(pipelineId, buildIdGuid, asset.Name, result, BuildTaskStatus.Failed);
+				await SendToMasterServerAsync(buildIdGuid, asset.Name, result, BuildTaskStatus.Failed);
 				return;
 			}
 			
@@ -111,31 +122,63 @@ public class RemoteBuildTargetRequest : IProcessable
 				deployProcess.Process();
 			
 			// tell master server build is completed
-			await SendToMasterServerAsync(pipelineId, buildIdGuid, asset.Name, result, result.IsErrors ? BuildTaskStatus.Failed : BuildTaskStatus.Succeed);
+			await SendToMasterServerAsync(buildIdGuid, asset.Name, result, result.IsErrors ? BuildTaskStatus.Failed : BuildTaskStatus.Succeed);
 		}
 		catch (Exception e)
 		{
 			Logger.Log(e);
-			var result = new BuildResult
-			{
-				BuildName = asset.Name,
-				Errors = $"{e.GetType().Name}: {e.Message}"
-			};
-			await SendToMasterServerAsync(pipelineId, buildIdGuid, asset.Name, result, BuildTaskStatus.Failed);
+			await SendExceptionToMaster(e, asset.Name, buildIdGuid);
 		}
 	}
 
-	private async Task SendToMasterServerAsync(ulong pipelineId, string? buildGuid, string buildName, BuildResult? buildResult, BuildTaskStatus status)
+	private async Task SendToMasterServerAsync(string? buildGuid, string buildName, BuildResult? buildResult, BuildTaskStatus status)
 	{
 		var response = new RemoteBuildResponse
 		{
-			PipelineId = pipelineId,
+			PipelineId = Packet.PipelineId,
 			BuildIdGuid = buildGuid,
 			BuildName = buildName,
 			BuildResult = buildResult,
 			Status = status
 		};
 
+		await SendToMasterAsync(response);
+	}
+
+	private async Task SendExceptionToMaster(Exception e, string assetName, string buildIdGuid)
+	{
+		var result = new BuildResult
+		{
+			BuildName = assetName,
+			Errors = $"{e.GetType().Name}: {e.Message}"
+		};
+		
+		var response = new RemoteBuildResponse
+		{
+			PipelineId = Packet.PipelineId,
+			BuildIdGuid = buildIdGuid,
+			BuildName = assetName,
+			BuildResult = result,
+			Status = BuildTaskStatus.Failed
+		};
+
+		await SendToMasterAsync(response);
+	}
+
+	private async Task SendExceptionToMaster(Exception e)
+	{
+		var result = new BuildResult { Errors = $"{e.GetType().Name}: {e.Message}" };
+		var response = new RemoteBuildResponse
+		{
+			PipelineId = Packet.PipelineId,
+			BuildResult = result,
+			Status = BuildTaskStatus.Failed
+		};
+		await SendToMasterAsync(response);
+	}
+	
+	private async Task SendToMasterAsync(RemoteBuildResponse response)
+	{
 		Logger.Log($"Sending build '{response.BuildIdGuid}' back to: {SendBackUrl}");
 		var body = new RemoteBuildPacket { BuildResponse = response };
 		await Web.SendAsync(HttpMethod.Post, SendBackUrl, body: body);
