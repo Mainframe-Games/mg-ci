56dc08d6951e004ec279ae401867203b0dbe8412
brogan89 <brogan.king@outlook.com>
2024-03-25 19:17:43 +1300

    Added image loader

--- AvaloniaAppMVVM/ViewModels/HomePageViewModel.cs
+++ AvaloniaAppMVVM/ViewModels/HomePageViewModel.cs
@@ -1,10 +1,25 @@
-﻿using AvaloniaAppMVVM.Data;
+﻿using System.Diagnostics;
+using Avalonia.Media.Imaging;
+using AvaloniaAppMVVM.Data;
+using AvaloniaAppMVVM.Utils;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AvaloniaAppMVVM.ViewModels;
 
 public partial class HomePageViewModel : ViewModelBase
 {
     [ObservableProperty]
-    private Project _project;
+    private Project? _project;
+
+    public Task<Bitmap?> ImageSourceBitmapWeb =>
+        ImageLoader.LoadFromWeb(
+            "https://cdn.cloudflare.steamstatic.com/steam/apps/1622570/header.jpg?t=1693947196"
+        );
+
+    [RelayCommand]
+    private void OpenStoreLinkCommand()
+    {
+        Process.Start("explorer", _project.Settings.StoreUrl);
+    }
 }
