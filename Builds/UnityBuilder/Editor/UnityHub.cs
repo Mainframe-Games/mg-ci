a5d5481c1d7615af8a4e2c3148044b355e24e314
brogan89 <brogan.king@outlook.com>
2024-04-01 09:42:50 +1300

    Create UnityHub.cs

---
+++ Builds/UnityBuilder/Editor/UnityHub.cs
@@ -0,0 +1,52 @@
+﻿using SharedLib;
+
+namespace UnityBuilder;
+
+/// <summary>
+/// docs: https://docs.unity3d.com/hub/manual/HubCLI.html
+/// </summary>
+public class UnityHub
+{
+    public static void Download()
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            DownloadWindows();
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            throw new NotImplementedException();
+        }
+        else if (OperatingSystem.IsMacOS())
+        {
+            throw new NotImplementedException();
+        }
+        else
+        {
+            throw new Exception("Unsupported Operating System");
+        }
+    }
+
+    private static void DownloadWindows()
+    {
+        var (exitCode, output) = Cmd.Run("winget", "install -e --id Unity.UnityHub");
+
+        if (exitCode != 0)
+        {
+            throw new Exception($"Failed to get Unity Hub. {output}");
+        }
+
+        Console.WriteLine("UnityHub installed successfully.");
+        Console.WriteLine(output);
+    }
+
+    public static void Help()
+    {
+        var (exitCode, output) = Cmd.Run(
+            "Unity Hub.exe",
+            "-- --headless help",
+            @"C:\Program Files\Unity Hub"
+        );
+        Console.WriteLine(output);
+    }
+}
