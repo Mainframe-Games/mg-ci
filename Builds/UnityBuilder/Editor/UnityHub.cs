ecf884e469759fb9bc7180f345b2b328f54c965d
brogan89 <brogan.king@outlook.com>
2024-04-01 10:05:09 +1300

    Update UnityHub.cs

--- Builds/UnityBuilder/Editor/UnityHub.cs
+++ Builds/UnityBuilder/Editor/UnityHub.cs
@@ -5,21 +5,40 @@ namespace UnityBuilder;
 /// <summary>
 /// docs: https://docs.unity3d.com/hub/manual/HubCLI.html
 /// </summary>
-public class UnityHub
+public static class UnityHub
 {
-    public static void Download()
+    private static string Path
+    {
+        get
+        {
+            if (OperatingSystem.IsWindows())
+                return @"C:\Program Files\Unity Hub\Unity Hub.exe";
+
+            if (OperatingSystem.IsMacOS())
+                return "/Applications/Unity Hub.app/Contents/MacOS/Unity Hub";
+
+            if (OperatingSystem.IsLinux())
+                return "/usr/bin/unityhub";
+
+            throw new Exception("Unsupported Operating System");
+        }
+    }
+
+    #region Install
+
+    public static void Install()
     {
         if (OperatingSystem.IsWindows())
         {
-            DownloadWindows();
-        }
-        else if (OperatingSystem.IsLinux())
-        {
-            throw new NotImplementedException();
+            InstallWindows();
         }
         else if (OperatingSystem.IsMacOS())
         {
-            throw new NotImplementedException();
+            InstallMac();
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            InstallLinux();
         }
         else
         {
@@ -27,7 +46,7 @@ public class UnityHub
         }
     }
 
-    private static void DownloadWindows()
+    private static void InstallWindows()
     {
         var (exitCode, output) = Cmd.Run("winget", "install -e --id Unity.UnityHub");
 
@@ -40,13 +59,46 @@ public class UnityHub
         Console.WriteLine(output);
     }
 
+    private static void InstallMac()
+    {
+        var (exitCode, output) = Cmd.Run("brew", "install --cask unity-hub");
+
+        if (exitCode != 0)
+        {
+            throw new Exception($"Failed to get Unity Hub. {output}");
+        }
+
+        Console.WriteLine("UnityHub installed successfully.");
+        Console.WriteLine(output);
+    }
+
+    /// <summary>
+    /// docs: https://docs.unity3d.com/hub/manual/InstallHub.html
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
+    private static void InstallLinux()
+    {
+        Cmd.Run(
+            "wget",
+            "-qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null"
+        );
+
+        Cmd.Run(
+            "sudo",
+            "sh -c 'echo \"deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main\" > /etc/apt/sources.list.d/unityhub.list'"
+        );
+
+        Cmd.Run("sudo", "apt update");
+        Cmd.Run("sudo", "apt-get install unityhub");
+
+        Console.WriteLine("UnityHub installed successfully.");
+    }
+
+    #endregion
+
     public static void Help()
     {
-        var (exitCode, output) = Cmd.Run(
-            "Unity Hub.exe",
-            "-- --headless help",
-            @"C:\Program Files\Unity Hub"
-        );
+        var (exitCode, output) = Cmd.Run(Path, "-- --headless help");
         Console.WriteLine(output);
     }
 }
