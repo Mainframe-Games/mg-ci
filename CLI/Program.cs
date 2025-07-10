c31f2ec1ab6d60cd9740b89eb31dfc52effedc78
brogan <brogan.king@outlook.com>
2025-07-10 12:21:59 +1200

    Add new CLI program

---
+++ CLI/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Spectre.Console;
+
+AnsiConsole.Write(
+    new FigletText("Mainframe CI")
+        .LeftJustified()
+        .Color(Color.Red));
